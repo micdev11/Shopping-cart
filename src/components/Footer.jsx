7645b2a8befa1679bb3255295c4a0baec987a587
Michael <michael.enriquez.mo@gmail.com>
2023-06-16 17:17:29 -0400

    initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      {/* <h5>Shopping Cart con useContext & useReducer</h5> */}
+    </footer>
+  )
+}
